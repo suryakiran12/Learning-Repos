772c24955ac27178d3dbb8e0583758adffd23b3d
skiran <skiran@akamai.com>
2023-09-02 11:58:21 +0530

    added two scripts

---
+++ shell-scripting/01-hello-world.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo
+echo "Hellow world,I am learning Devops with AWS"
+echo
+
+PERSON1=Sachin
+PERSON2=Rohit
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON1"
---
+++ shell-scripting/02-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#shell script run the command inside $() and
+#store the output in to a variable called DATE
+
+DATE=$(date)
+
+echo
+echo "Hello,this script is executed at timestamp: $DATE"
+echo
